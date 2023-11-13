d03ebee2fd85d9ee58c1b7bc4d102698e3b705df
iloozealot <iloozealot@gmail.com>
2023-11-13 09:56:57 -0600

    Add project files.

---
+++ LearningBasicIfStatements/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+//bool isComplete = false;
+
+//if (isComplete)
+//{
+//    Console.WriteLine("The statement was true.");
+//}
+//else
+//{
+//    Console.WriteLine("The statement was false.");
+//}
+
+using System.Threading.Channels;
+
+Console.Write("What is your first name? ");
+string? firstName = Console.ReadLine();
+
+if (firstName?.ToLower() == "justin")
+{
+    Console.WriteLine("Hello, Mr. Spencer.");
+}
+else
+{
+    Console.WriteLine($"Hello, {firstName}.");
+}
+Console.WriteLine("End of program.");
